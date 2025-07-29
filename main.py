9ec17ceca9e09ed82ad0b90025a9ff502837e959
Harsh Anand <harsh25061999@gmail.com>
2025-07-29 20:57:56 +0530

    dotenv module added to the project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
 client = OpenAI(
-    api_key = os.getenv("API_KEY"),
+    api_key = os.getenv("OPENAI_API_KEY"),
 )
 
 messages = []
